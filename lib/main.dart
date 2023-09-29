ba51609b14a88bfee4854bcfc29ae0dce06598da
BaltazarLuis <luis.baltazar@sellside.sl>
2023-09-29 10:36:14 -0500

    Login

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:firebase_ui_oauth_google/firebase_ui_oauth_google.dart';
+import 'package:flutter/material.dart';
+import 'app.dart';
+import 'firebase_options.dart'; 
+
+// TODO(codelab user): Get API key
+const clientId = 'YOUR_CLIENT_ID';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  FirebaseUIAuth.configureProviders([
+    EmailAuthProvider(),
+    GoogleProvider(clientId: '345587559400-thcl9p5qrjn1jq5kg92f61e19a9qnnm7.apps.googleusercontent.com'),
+  ]);
+
+  runApp(const MyApp());
+}
